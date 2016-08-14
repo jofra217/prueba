3a4d0fa1c965af146e860208662a899c505067de
Jofra Montesdeoca Nuez <joframontesdeocanuez.b@gmail.com>
2016-08-14 12:33:18 +0100

    Initial Commit

---
+++ Hola/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Hola
+//
+//  Created by Jofra Montesdeoca Nuez on 14/8/16.
+//  Copyright © 2016 Jofra Montesdeoca Nuez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Hola/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Hola
+//
+//  Created by Jofra Montesdeoca Nuez on 14/8/16.
+//  Copyright © 2016 Jofra Montesdeoca Nuez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
